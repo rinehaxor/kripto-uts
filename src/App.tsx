cc0ce0300d3bef5915d203c41994c1057bb7688d
rinehaxor <rinehaxor@users.noreply.github.com>
2023-11-26 12:07:06 +0700

    fix:decrypt

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
 // App.tsx
-import { Table, TableBody, TableCell, TableRow } from "./component/Table";
-import { abjad } from "./constant/abjad";
-import HillCipher from "./HillCipher";
-import HillCipherDecrypt from "./HillCipherDecrypt";
+import { Table, TableBody, TableCell, TableRow } from './component/Table';
+import { abjad } from './constant/abjad';
+import HillCipher from './HillCipher';
+import HillCipherDecryptor from './HillCipherDecrypt';
+import HillCipherDecrypt from './HillCipherDecrypt';
 
 function App() {
-	return (
-		<div className="container mx-auto max-w-7xl">
-			<h1 className="text-3xl font-bold text-center mt-10">Hill Cipher</h1>
-			<Table className="my-10">
-				<TableBody>
-					<TableRow>
-						{abjad.map((item, index) => {
-							return <TableCell key={index}>{item}</TableCell>;
-						})}
-					</TableRow>
-					<TableRow>
-						{abjad.map((_item, index) => {
-							return <TableCell key={index}>{index}</TableCell>;
-						})}
-					</TableRow>
-				</TableBody>
-			</Table>
-			<div className="w-full flex ">
-				<HillCipher />
-				<HillCipherDecrypt />
-			</div>
-		</div>
-	);
+   return (
+      <div className="container mx-auto max-w-7xl">
+         <h1 className="text-3xl font-bold text-center mt-10">Hill Cipher</h1>
+         <Table className="my-10">
+            <TableBody>
+               <TableRow>
+                  {abjad.map((item, index) => {
+                     return <TableCell key={index}>{item}</TableCell>;
+                  })}
+               </TableRow>
+               <TableRow>
+                  {abjad.map((_item, index) => {
+                     return <TableCell key={index}>{index}</TableCell>;
+                  })}
+               </TableRow>
+            </TableBody>
+         </Table>
+         <div className="w-full flex ">
+            <HillCipher />
+            <HillCipherDecryptor />
+         </div>
+      </div>
+   );
 }
 
 export default App;
--- src/HillCipherDecrypt.tsx
+++ src/HillCipherDecrypt.tsx
@@ -1,182 +1,101 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { create, all } from "mathjs";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from "./component/Form";
-import { Input } from "./component/Input";
-import { Button } from "./component/Button";
+import React, { useState } from 'react';
 
-const HillCipherDecrypt = () => {
-	const [decryptText, setdecryptText] = useState<string>("");
-	const math = create(all);
+function HillCipherDecryptor() {
+   const [encryptedText, setEncryptedText] = useState('');
+   const [decryptedText, setDecryptedText] = useState('');
 
-	// menghitung koefisien kofaktor dari sebuah matriks.
-	// Fungsi ini menerima parameter berupa matriks mat, dan dua indeks p dan q yang mewakili baris dan kolom elemen yang kofaktornya sedang dihitung.
-	// Fungsi ini membuat sebuah matriks sementara temp dengan menghapus baris p dan kolom q dari mat.
-	// Kemudian, fungsi menghitung determinan dari temp
-	// Koefisien kofaktor diperoleh dengan mengalikan determinan dengan nilai 1 atau -1, tergantung pada genap atau ganjilnya jumlah dari p dan q.
-	// Nilai koefisien kofaktor kemudian dikembalikan.
-	const getCofactor = (mat: number[][], p: number, q: number) => {
-		const size = mat.length;
-		const temp = [];
+   // Contoh matriks invers kunci (anda harus menghitungnya sesuai dengan kunci Anda)
+   const inverseKey = [
+      [2, 8, 15],
+      [7, 4, 17],
+      [8, 13, 6],
+   ];
+   function mod(n, m) {
+      return ((n % m) + m) % m;
+   }
+   function modInverse(a, m) {
+      a = mod(a, m);
+      for (let x = 1; x < m; x++) {
+         if (mod(a * x, m) === 1) {
+            return x;
+         }
+      }
+      return 1; // Matriks kunci harus memiliki invers
+   }
 
-		for (let row = 0; row < size; row++) {
-			const currentRow = [];
-			for (let col = 0; col < size; col++) {
-				if (row !== p && col !== q) {
-					currentRow.push(mat[row][col]);
-				}
-			}
-			if (currentRow.length > 0) {
-				temp.push(currentRow);
-			}
-		}
+   function charToInt(c) {
+      return c.charCodeAt(0) - 'A'.charCodeAt(0);
+   }
 
-		const cofactor = math.det(temp);
-		return ((p + q) % 2 === 0 ? 1 : -1) * cofactor;
-	};
+   function intToChar(i) {
+      return String.fromCharCode((i % 26) + 'A'.charCodeAt(0));
+   }
 
-	// Kode ini mendefinisikan sebuah fungsi bernama getCofactorMatrix yang menerima sebuah matriks mat sebagai input dan mengembalikan matriks kofaktor dari mat.
-	// Matriks kofaktor adalah matriks persegi yang berisi kofaktor dari setiap elemen di mat.
-	// Fungsi ini melakukan iterasi pada setiap elemen di mat dan menghitung kofaktornya menggunakan fungsi getCofactor.
-	// Kemudian, kofaktor tersebut ditambahkan ke baris saat ini dari matriks kofaktor.
-	// Terakhir, fungsi ini mengembalikan matriks kofaktor.
-	const getCofactorMatrix = (mat: number[][]) => {
-		const size = mat.length;
-		const cofactorMat = [];
+   function decryptHillCipher(encryptedText, keyMatrix) {
+      let decryptedText = '';
 
-		for (let row = 0; row < size; row++) {
-			const currentRow = [];
-			for (let col = 0; col < size; col++) {
-				const cofactor = getCofactor(mat, row, col);
-				currentRow.push(cofactor);
-			}
-			cofactorMat.push(currentRow);
-		}
+      // Menghitung determinan matriks kunci
+      let det =
+         keyMatrix[0][0] * (keyMatrix[1][1] * keyMatrix[2][2] - keyMatrix[1][2] * keyMatrix[2][1]) -
+         keyMatrix[0][1] * (keyMatrix[1][0] * keyMatrix[2][2] - keyMatrix[1][2] * keyMatrix[2][0]) +
+         keyMatrix[0][2] * (keyMatrix[1][0] * keyMatrix[2][1] - keyMatrix[1][1] * keyMatrix[2][0]);
 
-		return cofactorMat;
-	};
+      let detInv = modInverse(det, 26);
 
-	//Kode ini mendefinisikan sebuah fungsi bernama getAdjugateMatrix yang menerima matriks mat sebagai input.
-	// Fungsi ini menghitung matriks kofaktor menggunakan fungsi getCofactorMatrix
-	// kemudian mengambil transpose dari matriks kofaktor.
-	//Terakhir, fungsi ini mengembalikan matriks adjoin.
-	const getAdjugateMatrix = (mat: number[][]) => {
-		const cofactorMat = getCofactorMatrix(mat);
-		const adjugateMat = math.transpose(cofactorMat);
-		return adjugateMat;
-	};
+      // Menghitung invers matriks
+      let adj = [
+         [
+            (keyMatrix[1][1] * keyMatrix[2][2] - keyMatrix[2][1] * keyMatrix[1][2]) % 26,
+            (keyMatrix[0][2] * keyMatrix[2][1] - keyMatrix[0][1] * keyMatrix[2][2]) % 26,
+            (keyMatrix[0][1] * keyMatrix[1][2] - keyMatrix[0][2] * keyMatrix[1][1]) % 26,
+         ],
+         [
+            (keyMatrix[1][2] * keyMatrix[2][0] - keyMatrix[1][0] * keyMatrix[2][2]) % 26,
+            (keyMatrix[0][0] * keyMatrix[2][2] - keyMatrix[0][2] * keyMatrix[2][0]) % 26,
+            (keyMatrix[1][0] * keyMatrix[0][2] - keyMatrix[0][0] * keyMatrix[1][2]) % 26,
+         ],
+         [
+            (keyMatrix[1][0] * keyMatrix[2][1] - keyMatrix[2][0] * keyMatrix[1][1]) % 26,
+            (keyMatrix[2][0] * keyMatrix[0][1] - keyMatrix[0][0] * keyMatrix[2][1]) % 26,
+            (keyMatrix[0][0] * keyMatrix[1][1] - keyMatrix[1][0] * keyMatrix[0][1]) % 26,
+         ],
+      ];
 
-	const adjugateMatrix = getAdjugateMatrix([
-		[2, 8, 15],
-		[7, 4, 17],
-		[8, 13, 6],
-	]);
+      let inverseMatrix = adj.map((row) => row.map((el) => mod(el * detInv, 26)));
 
-	const x = 5;
-	const productMatrix = adjugateMatrix.map((row) =>
-		row.map((element) => math.mod(math.multiply(element, x), 26))
-	);
+      for (let i = 0; i < encryptedText.length; i += 3) {
+         let vector = [0, 0, 0];
+         for (let j = 0; j < 3; j++) {
+            vector[j] = charToInt(encryptedText[i + j]);
+         }
 
-	// Fungsi untuk mengalikan matriks kunci dengan vektor teks
-	// memiliki 2 param key dan messageVector yang digunakan untuk menambil array 1 diemensi lalu dikalikan dengan key
-	// key menggunakan array 2 dimensi sebagai contoh [[1, 2], [3, 4],[5,6]] array didalam array
-	// result adalah vektor setiap elemennya adalah jumlah modulo 26
-	const multiplyMatrixWithVector = (
-		key: number[][],
-		messageVector: number[]
-	): number[] => {
-		const result: number[] = new Array(messageVector.length).fill(0);
+         let resultVector = [0, 0, 0];
+         for (let j = 0; j < 3; j++) {
+            for (let k = 0; k < 3; k++) {
+               resultVector[j] = mod(resultVector[j] + inverseMatrix[j][k] * vector[k], 26);
+            }
+            resultVector[j] = intToChar(resultVector[j]);
+         }
 
-		for (let i = 0; i < messageVector.length; i++) {
-			for (let j = 0; j < messageVector.length; j++) {
-				result[i] += key[i][j] * messageVector[j];
-			}
-			result[i] %= 26;
-		}
+         decryptedText += resultVector.join('');
+      }
 
-		return result;
-	};
+      return decryptedText;
+   }
 
-	// menerima 1 parameter teks yang dirubah menjadi array 1 dimensi dan menjadi huruf besar semua dengan toUppercase
-	// split untuk memecah isi dari input contoh ABCD menjadi [A,B,C,D]
-	//  lalu di maping ap((char) => char.charCodeAt(0) - 'A'.charCodeAt(0)):  fungsi tersebut. Untuk setiap karakter dalam array
-	// 'A'.charCodeAt(0): Mengonversi  'A' menjadi kode ASCII-nya, nilai default 65.
-	// char.charCodeAt(0) - 'A'.charCodeAt(0): Dengan mengurangi nilai ASCII dari 'A' dari setiap karakter, kita mengubah alfabet menjadi range 0-25 (dimana 'A' = 0, 'B' = 1, dst.).
-	const stringToVector = (text: string): number[] => {
-		return text
-			.toUpperCase()
-			.split("")
-			.map((char) => char.charCodeAt(0) - "A".charCodeAt(0));
-	};
+   const handleDecrypt = () => {
+      const result = decryptHillCipher(encryptedText, inverseKey);
+      setDecryptedText(result);
+   };
 
-	// menerima satu parameter vec dengan format angka number array[]
-	// lalu maping array yang diambil dari nilai i
-	// String.fromCharCode(i + 'A'.charCodeAt(0)):  ini mengonversi setiap angka dalam array kembali menjadi karakter.
-	// i + 'A'.charCodeAt(0): Karena dalam proses enkripsi setiap huruf diubah menjadi angka 0-25,   menambahkan ini dengan 65 untuk mendapatkan kembali kode Ascci menggunakan String.fromCharCode(
-	const vectorToString = (vec: number[]): string => {
-		return vec.map((i) => String.fromCharCode(i + "A".charCodeAt(0))).join("");
-	};
+   return (
+      <div>
+         <h2>Hill Cipher Decryptor</h2>
+         <input type="text" value={encryptedText} onChange={(e) => setEncryptedText(e.target.value)} placeholder="Enter encrypted text" />
+         <button onClick={handleDecrypt}>Decrypt</button>
+         <p>Decrypted Text: {decryptedText}</p>
+      </div>
+   );
+}
 
-	// variabel key yang berisi array 2 dimensi yaitu matriks yang diberikan dibawah ini adalah 3x3
-	const decrypt = (ciphertext: string) => {
-		const ciphertextVector = stringToVector(ciphertext); // mengonversi string plaintext menjadi vektor numerik. Setiap huruf diubah menjadi nilai numeriknya sesuai posisi dalam alfabet (A=0, B=1, ..., Z=25
-		const decryptVector = multiplyMatrixWithVector(
-			productMatrix,
-			ciphertextVector
-		); //mengalikan matriks kunci dengan vektor plaintext. perkalian ini (dilakukan dalam aritmetika modulo) menghasilkan vektor baru yang mewakili teks yang telah dienkripsi.
-		setdecryptText(vectorToString(decryptVector)); //mengonversi vektor numerik yang dihasilkan kembali menjadi string teks
-	};
-
-	const FormSchema = z.object({
-		ciphertext: z
-			.string()
-			.min(3, {
-				message: "Harus 3 Huruf",
-			})
-			.max(3, {
-				message: "Maksimal 3 Huruf",
-			}),
-	});
-	const form = useForm<z.infer<typeof FormSchema>>({
-		resolver: zodResolver(FormSchema),
-		defaultValues: {
-			ciphertext: "",
-		},
-	});
-	function onSubmit(data: z.infer<typeof FormSchema>) {
-		decrypt(data.ciphertext);
-	}
-	return (
-		<div className="w-full">
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-					<FormField
-						control={form.control}
-						name="ciphertext"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Enkripsi</FormLabel>
-								<FormControl>
-									<Input placeholder="Cipher Text" {...field} maxLength={3} />
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<Button type="submit">Dekrip</Button>
-					<p>{decryptText}</p>
-				</form>
-			</Form>
-		</div>
-	);
-};
-export default HillCipherDecrypt;
+export default HillCipherDecryptor;
