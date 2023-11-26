ce6b6b28f3ab21c996994c44dad9dbe464b5c8a1
dimasandriano <dimas.andriano.h@gmail.com>
2023-11-26 12:23:40 +0700

    fix: type

--- src/HillCipherDecrypt.tsx
+++ src/HillCipherDecrypt.tsx
@@ -1,101 +1,126 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 
 function HillCipherDecryptor() {
-   const [encryptedText, setEncryptedText] = useState('');
-   const [decryptedText, setDecryptedText] = useState('');
+	const [encryptedText, setEncryptedText] = useState("");
+	const [decryptedText, setDecryptedText] = useState("");
 
-   // Contoh matriks invers kunci (anda harus menghitungnya sesuai dengan kunci Anda)
-   const inverseKey = [
-      [2, 8, 15],
-      [7, 4, 17],
-      [8, 13, 6],
-   ];
-   function mod(n, m) {
-      return ((n % m) + m) % m;
-   }
-   function modInverse(a, m) {
-      a = mod(a, m);
-      for (let x = 1; x < m; x++) {
-         if (mod(a * x, m) === 1) {
-            return x;
-         }
-      }
-      return 1; // Matriks kunci harus memiliki invers
-   }
+	// Contoh matriks invers kunci (anda harus menghitungnya sesuai dengan kunci Anda)
+	const inverseKey = [
+		[2, 8, 15],
+		[7, 4, 17],
+		[8, 13, 6],
+	];
+	function mod(n: number, m: number) {
+		return ((n % m) + m) % m;
+	}
+	function modInverse(a: number, m: number) {
+		a = mod(a, m);
+		for (let x = 1; x < m; x++) {
+			if (mod(a * x, m) === 1) {
+				return x;
+			}
+		}
+		return 1; // Matriks kunci harus memiliki invers
+	}
 
-   function charToInt(c) {
-      return c.charCodeAt(0) - 'A'.charCodeAt(0);
-   }
+	function charToInt(c: string) {
+		return c.charCodeAt(0) - "A".charCodeAt(0);
+	}
 
-   function intToChar(i) {
-      return String.fromCharCode((i % 26) + 'A'.charCodeAt(0));
-   }
+	function intToChar(i: number) {
+		console.log(String.fromCharCode((i % 26) + "A".charCodeAt(0)));
 
-   function decryptHillCipher(encryptedText, keyMatrix) {
-      let decryptedText = '';
+		return String.fromCharCode((i % 26) + "A".charCodeAt(0));
+	}
 
-      // Menghitung determinan matriks kunci
-      let det =
-         keyMatrix[0][0] * (keyMatrix[1][1] * keyMatrix[2][2] - keyMatrix[1][2] * keyMatrix[2][1]) -
-         keyMatrix[0][1] * (keyMatrix[1][0] * keyMatrix[2][2] - keyMatrix[1][2] * keyMatrix[2][0]) +
-         keyMatrix[0][2] * (keyMatrix[1][0] * keyMatrix[2][1] - keyMatrix[1][1] * keyMatrix[2][0]);
+	function decryptHillCipher(encryptedText: string, keyMatrix: number[][]) {
+		let decryptedText = "";
 
-      let detInv = modInverse(det, 26);
+		// Menghitung determinan matriks kunci
+		const det =
+			keyMatrix[0][0] *
+				(keyMatrix[1][1] * keyMatrix[2][2] - keyMatrix[1][2] * keyMatrix[2][1]) -
+			keyMatrix[0][1] *
+				(keyMatrix[1][0] * keyMatrix[2][2] - keyMatrix[1][2] * keyMatrix[2][0]) +
+			keyMatrix[0][2] *
+				(keyMatrix[1][0] * keyMatrix[2][1] - keyMatrix[1][1] * keyMatrix[2][0]);
 
-      // Menghitung invers matriks
-      let adj = [
-         [
-            (keyMatrix[1][1] * keyMatrix[2][2] - keyMatrix[2][1] * keyMatrix[1][2]) % 26,
-            (keyMatrix[0][2] * keyMatrix[2][1] - keyMatrix[0][1] * keyMatrix[2][2]) % 26,
-            (keyMatrix[0][1] * keyMatrix[1][2] - keyMatrix[0][2] * keyMatrix[1][1]) % 26,
-         ],
-         [
-            (keyMatrix[1][2] * keyMatrix[2][0] - keyMatrix[1][0] * keyMatrix[2][2]) % 26,
-            (keyMatrix[0][0] * keyMatrix[2][2] - keyMatrix[0][2] * keyMatrix[2][0]) % 26,
-            (keyMatrix[1][0] * keyMatrix[0][2] - keyMatrix[0][0] * keyMatrix[1][2]) % 26,
-         ],
-         [
-            (keyMatrix[1][0] * keyMatrix[2][1] - keyMatrix[2][0] * keyMatrix[1][1]) % 26,
-            (keyMatrix[2][0] * keyMatrix[0][1] - keyMatrix[0][0] * keyMatrix[2][1]) % 26,
-            (keyMatrix[0][0] * keyMatrix[1][1] - keyMatrix[1][0] * keyMatrix[0][1]) % 26,
-         ],
-      ];
+		const detInv = modInverse(det, 26);
 
-      let inverseMatrix = adj.map((row) => row.map((el) => mod(el * detInv, 26)));
+		// Menghitung invers matriks
+		const adj = [
+			[
+				(keyMatrix[1][1] * keyMatrix[2][2] - keyMatrix[2][1] * keyMatrix[1][2]) %
+					26,
+				(keyMatrix[0][2] * keyMatrix[2][1] - keyMatrix[0][1] * keyMatrix[2][2]) %
+					26,
+				(keyMatrix[0][1] * keyMatrix[1][2] - keyMatrix[0][2] * keyMatrix[1][1]) %
+					26,
+			],
+			[
+				(keyMatrix[1][2] * keyMatrix[2][0] - keyMatrix[1][0] * keyMatrix[2][2]) %
+					26,
+				(keyMatrix[0][0] * keyMatrix[2][2] - keyMatrix[0][2] * keyMatrix[2][0]) %
+					26,
+				(keyMatrix[1][0] * keyMatrix[0][2] - keyMatrix[0][0] * keyMatrix[1][2]) %
+					26,
+			],
+			[
+				(keyMatrix[1][0] * keyMatrix[2][1] - keyMatrix[2][0] * keyMatrix[1][1]) %
+					26,
+				(keyMatrix[2][0] * keyMatrix[0][1] - keyMatrix[0][0] * keyMatrix[2][1]) %
+					26,
+				(keyMatrix[0][0] * keyMatrix[1][1] - keyMatrix[1][0] * keyMatrix[0][1]) %
+					26,
+			],
+		];
 
-      for (let i = 0; i < encryptedText.length; i += 3) {
-         let vector = [0, 0, 0];
-         for (let j = 0; j < 3; j++) {
-            vector[j] = charToInt(encryptedText[i + j]);
-         }
+		const inverseMatrix = adj.map((row) => row.map((el) => mod(el * detInv, 26)));
 
-         let resultVector = [0, 0, 0];
-         for (let j = 0; j < 3; j++) {
-            for (let k = 0; k < 3; k++) {
-               resultVector[j] = mod(resultVector[j] + inverseMatrix[j][k] * vector[k], 26);
-            }
-            resultVector[j] = intToChar(resultVector[j]);
-         }
+		for (let i = 0; i < encryptedText.length; i += 3) {
+			const vector = [0, 0, 0];
+			for (let j = 0; j < 3; j++) {
+				vector[j] = charToInt(encryptedText[i + j]);
+			}
 
-         decryptedText += resultVector.join('');
-      }
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			const resultVector: any[] = [0, 0, 0];
+			console.log({ resultVector });
 
-      return decryptedText;
-   }
+			for (let j = 0; j < 3; j++) {
+				for (let k = 0; k < 3; k++) {
+					resultVector[j] = mod(
+						resultVector[j] + inverseMatrix[j][k] * vector[k],
+						26
+					);
+				}
+				resultVector[j] = intToChar(resultVector[j]);
+			}
 
-   const handleDecrypt = () => {
-      const result = decryptHillCipher(encryptedText, inverseKey);
-      setDecryptedText(result);
-   };
+			decryptedText += resultVector.join("");
+		}
 
-   return (
-      <div>
-         <h2>Hill Cipher Decryptor</h2>
-         <input type="text" value={encryptedText} onChange={(e) => setEncryptedText(e.target.value)} placeholder="Enter encrypted text" />
-         <button onClick={handleDecrypt}>Decrypt</button>
-         <p>Decrypted Text: {decryptedText}</p>
-      </div>
-   );
+		return decryptedText;
+	}
+
+	const handleDecrypt = () => {
+		const result = decryptHillCipher(encryptedText, inverseKey);
+		setDecryptedText(result);
+	};
+
+	return (
+		<div>
+			<h2>Hill Cipher Decryptor</h2>
+			<input
+				type="text"
+				value={encryptedText}
+				onChange={(e) => setEncryptedText(e.target.value)}
+				placeholder="Enter encrypted text"
+			/>
+			<button onClick={handleDecrypt}>Decrypt</button>
+			<p>Decrypted Text: {decryptedText}</p>
+		</div>
+	);
 }
 
 export default HillCipherDecryptor;
